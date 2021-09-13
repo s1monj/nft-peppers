e0375764dd502806575853ba14da6658db2397a7
s1monj <simon.leigh.johnson@gmail.com>
2021-09-13 12:58:26 -0400

    graph animation

--- cyto/peppers.js
+++ cyto/peppers.js
@@ -1,22 +1,57 @@
-const TARGET=
+const TARGET_ID = "target"
+
+const GROWTH_SPEED = 10000;
+const FRUIT_HEIGHT = 7.6; //cm
+const BIRTHDAY_OF_MONTH = 3;
 
 document.addEventListener("DOMContentLoaded", function () {
+
   //var geneData = dataChinense;
   //var geneData = dataBaccatum;
-  var geneData = dataAnnuum;
+  const geneData = dataAnnuum;
 
+  // Build the stage
+  // Implement in JS so there's no depenedency on html or css
+  const target = document.getElementById(TARGET_ID);
 
+  const container = document.createElement('div');
+  container.setAttribute("id","container");
+  container.style.height="100%";
+  container.style.width="100%";
+  container.style.backgroundColor="#FCF5E5";
+  container.style.display="flex";
+  container.style.flexFlow="row nowrap";
+  target.appendChild(container);
 
+  // Overlay without absolute positioning:
+  // https://stackoverflow.com/a/48877138/4338238
 
-  var cy = cytoscape({
-    container: document.getElementById("canvas"), // container to render in
+  const graph1 = document.createElement('div');
+  graph1.setAttribute("id","graph1")
+  graph1.style.boxSizing="border-box";
+  graph1.style.width="100%";
+  graph1.style.flex="none";
+  graph1.style.borderRadius="100%";
+  container.appendChild(graph1)
+
+  const graph2 = document.createElement('div');
+  graph2.setAttribute("id","graph2")
+  graph2.style.boxSizing="border-box";
+  graph2.style.width="100%";
+  graph2.style.flex="none";
+  graph2.style.marginLeft="-100%";
+  graph2.style.borderRadius="100%";
+  container.appendChild(graph2)
+
+  // Display the graphs
+  const cyto1 = cytoscape({
+    container: document.getElementById("graph1"),
     elements: geneData,
     style: [
       {
         selector: "node",
         style: {
           "background-color": "#666",
-          
         },
       },
       {
@@ -33,25 +68,17 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-  let animation = anime({
-    targets: '#canvas',
-    rotate: [180, 160],
-    duration: 20000,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-  });
+  const startRotation = randomIntFromInterval(0,360);
+  grow("#graph1",startRotation,1)
 
-
-  var cy2 = cytoscape({
-    container: document.getElementById("canvas2"), // container to render in
+  const cyto2 = cytoscape({
+    container: document.getElementById("graph2"), // container to render in
     elements: geneData,
     style: [
       {
         selector: "node",
         style: {
           "background-color": "#363",
-          
         },
       },
       {
@@ -68,24 +95,54 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-  let animation2 = anime({
-    targets: '#canvas2',
-    rotate: [180, 200],
-    duration: 20000,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true,
-  })
-
-  // let animation3 = anime({
-  //   targets: '#canvas2'
-  //   skewY: [0,-10],
-  //   duration: 10000,
-  //   easing: 'easeInOutSine',
-  //   direction: 'alternate',
-  //   loop: true
-  // });
+  grow("#graph2",startRotation,-1)
 
 });
+
+function grow(target, startRotation, n){
+  anime({
+    targets: target,
+    rotate: startRotation,
+    duration: 1,
+    easing: 'easeInOutSine',
+    complete: function(){ skew(target, startRotation, n) }
+  });
+}
+
+function skew(target, startRotation, n){
+  const skew = FRUIT_HEIGHT*randomIntFromInterval(0.5,5)*n;
+  anime({
+    targets: target,
+    skew: [skew,0],
+    duration: (GROWTH_SPEED/2)+randomIntFromInterval(100,200),
+    complete: function(){startSwing(target, startRotation, n);}
+  });
+}
+
+function startSwing(target, startRotation, n){
+  const swingSpeed = (GROWTH_SPEED*2)+randomIntFromInterval(100,1000);
+  anime({
+    targets: target,
+    rotate: [startRotation, startRotation+(10*n)],
+    duration: swingSpeed/5,
+    easing: 'easeInOutSine',
+    complete: function(){swing(target, startRotation, swingSpeed, n)}
+  });
+}
+
+function swing(target, startRotation, swingSpeed, n){
+  anime({
+    targets: target,
+    rotate: [startRotation+(10*n),startRotation-(10*n)],
+    duration: swingSpeed/5.5,
+    easing: 'easeInOutSine',
+    direction: 'alternate',
+    loop: true
+  });
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 
